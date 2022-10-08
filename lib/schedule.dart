75c6de41a0f63d1440a984782ab9cc98baa80fa5
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-10-08 18:42:16 +0900

    feat: refactor Schedule class constructors

--- lib/schedule.dart
+++ lib/schedule.dart
@@ -1,15 +1,24 @@
 /// アプリで管理・操作するスケジュール。
 class Schedule {
-  Schedule.fromJson(Map<String, dynamic> json)
-      : scheduleId = (json['scheduleId'] ?? '') as String,
-        title = (json['title'] ?? '') as String,
-        createdAt = DateTime.parse(json['createdAt']),
-        dueDateTime = DateTime.parse(json['dueDateTime']),
-        isNotified = json['isNotified'] ?? false;
+  Schedule({
+    required this.scheduleId,
+    required this.title,
+    required this.dueDateTime,
+    this.isNotified = false,
+    this.createdAt,
+  });
 
   final String scheduleId;
   final String title;
-  final DateTime createdAt;
   final DateTime dueDateTime;
   final bool isNotified;
+  final DateTime? createdAt;
+
+  factory Schedule.fromJson(Map<String, dynamic> json) => Schedule(
+        scheduleId: (json['scheduleId'] ?? '') as String,
+        title: (json['title'] ?? '') as String,
+        dueDateTime: DateTime.parse(json['dueDateTime']),
+        isNotified: json['isNotified'] ?? false,
+        createdAt: DateTime.tryParse(json['createdAt']),
+      );
 }
