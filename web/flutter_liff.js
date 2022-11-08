b8f241d611200fb9097c1d13b79231c7ba65d00c
heyhey1028 <shoheiogawa1028@gmail.com>
2022-11-08 17:24:48 +0900

    chore: delete unnecessary log outputs

--- web/flutter_liff.js
+++ web/flutter_liff.js
@@ -1,6 +1,5 @@
 async function init(config){
     console.log("init called")
-    console.log(`liff Id: ${config["liffId"]}`)
     await liff.init({
         liffId: config["liffId"],
         withLoginOnEternalBrowser: true,
@@ -19,22 +18,17 @@ function getLiffId(){
 
 async function getUserId(){
     console.log("getUserId called")
-    console.log(`liff Id is ${liff.id}`)
    const profile = await liff.getProfile()
-   console.log(`profile: ${profile}`)
    return profile["userId"]
 }
 
 async function getGroupId(){
     console.log("getGroupId called")
-    console.log(`liff Id is ${liff.id}`)
    const context = await liff.getContext()
-   console.log(`context: ${JSON.stringify(context, null, 2)}`)
    return context["groupId"]
 }
 
 async function getAccessToken(){
     console.log("getAccessToken called")
-    console.log(`liff Id is ${liff.id}`)
     return await liff.getAccessToken()
 }
